2de4ff99f4a6a8ad2511249b5b232b951ff17052
black cat <sppam@163.com>
2016-01-28 13:43:07 +0800

    Initial Commit

---
+++ haha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  haha
+//
+//  Created by 张鹏宇 on 16/1/28.
+//  Copyright © 2016年 sppam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
